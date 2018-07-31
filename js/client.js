9f8f4d6251e35e1c1fbb1023d7f3f06ef9043d1f
Mohd Akram Ansari <mdakram28@gmail.com>
2018-07-31 12:32:11 +0530

    sdakfgh

---
+++ js/client.js
@@ -0,0 +1,38 @@
+function initClient(done) {
+	console.log("Init AgoraRTC client with App ID: " + appId);
+	client = AgoraRTC.createClient({
+		mode: 'interop'
+	});
+	client.init(appId, function () {
+		console.log("AgoraRTC client initialized");
+		client.join(channel_key, channel_value, null, function (uid) {
+			console.log("User " + uid + " join channel successfully");
+			done(client, uid);
+		});
+	}, function (err) {
+		console.log("AgoraRTC client init failed", err);
+	});
+
+	client.on('error', function (err) {
+		console.log("Got error msg:", err.reason);
+		if (err.reason === 'DYNAMIC_KEY_TIMEOUT') {
+			client.renewChannelKey(channelKey, function () {
+				console.log("Renew channel key successfully");
+			}, function (err) {
+				console.log("Renew channel key failed: ", err);
+			});
+		}
+	});
+
+}
+
+
+function init() {
+	getLocalStreams(function (localStreams) {
+		initClient(function (client, uid) {
+			publishAllStreams(client, localStreams);
+		});
+	});
+}
+
+init();
---
+++ js/common.js
@@ -0,0 +1,93 @@
+if (!AgoraRTC.checkSystemRequirements()) {
+	alert("Your browser does not support WebRTC!");
+}
+
+/* simulated data to proof setLogLevel() */
+AgoraRTC.Logger.error('this is error');
+AgoraRTC.Logger.warning('this is warning');
+AgoraRTC.Logger.info('this is info');
+AgoraRTC.Logger.debug('this is debug');
+
+var client, localStream, camera, microphone;
+// var cameras;
+var channel_key = null;
+var channel_value = 1000;
+var appId = "";
+
+function createLocalStream(streamId, cameraId, onInit) {
+	var localStream = AgoraRTC.createStream({
+		streamID: streamId,
+		audio: false,
+		cameraId: cameraId,
+		// microphoneId: microphone,
+		video: true,
+		screen: false
+	});
+	localStream.setVideoProfile('720p_3');
+	localStream.on("accessAllowed", function () {
+		console.log("accessAllowed");
+	});
+	localStream.on("accessDenied", function () {
+		console.log("accessDenied");
+	});
+	localStream.init(function () {
+		console.log("getUserMedia successfully");
+		// localStream.play('video' + displayId);
+		getVideoFrame(streamId);
+		localStream.play(streamId);
+		if (onInit) onInit();
+	}, function (err) {
+		console.log("getUserMedia failed", err);
+	});
+}
+
+function getVideoFrame(videoId) {
+	if($("#videos .video-box#"+videoId).length === 0) {
+		$("#video-box-template").clone().attr("id", videoId).appendTo("#videos");
+	}
+
+	return ("#videos .video-box#"+videoId);
+}
+
+function removeVideoFrame(videoId) {
+	$("#videos .video-box#"+videoId).remove();
+}
+
+function generateUID() {
+	return Math.ceil(Math.random() * 100000000);
+}
+
+function getLocalStreams(done) {
+	AgoraRTC.getDevices(function (all_devices) {
+		console.log(all_devices);
+		var cameras = all_devices.filter(function (device) {
+			return device.kind == "videoinput";
+		});
+		doneCount = 0;
+		var localStreams = [];
+		cameras.forEach(function (camera) {
+			localStreams.push(createLocalStream(generateUID(), camera.deviceId, function () {
+				doneCount++;
+				if (doneCount >= cameras.length) {
+					done(localStreams);
+				}
+			}));
+		});
+	});
+}
+
+function publishStream(client, localStream) {
+	client.publish(localStream, function (err) {
+		console.log("Publish local stream error: " + err);
+	});
+
+	client.on('stream-published', function (evt) {
+		console.log("Publish local stream successfully");
+	});
+}
+
+function publishAllStreams(client, localStreams) {
+	localStreams.forEach(function (localStream) {
+		publishStream(client, localStream);
+	});
+}
