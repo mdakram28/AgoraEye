b0e9e75be9ea1180a31d07c942d5d5091dde3361
Mohd Akram Ansari <mdakram28@gmail.com>
2018-07-31 14:00:17 +0530

    admin

--- js/common.js
+++ js/common.js
@@ -9,7 +9,7 @@ AgoraRTC.Logger.info('this is info');
 AgoraRTC.Logger.debug('this is debug');
 
 var channel_key = null;
-var channel_value = 1000;
+var channel_value = "1000";
 var appId = "731a9f30d6c5463c9710216fa61de348";
 
 function createLocalStream(streamId, cameraId, onInit) {
